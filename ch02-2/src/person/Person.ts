dedf942be18f15bb1c8c3eac21ce0684b93e0e20
seoyun-lee <20212025@o365.hanbat.ac.kr>
2023-09-05 20:04:56 +0900

    typescript ch02 : 프로젝트 생성과 관리

---
+++ ch02-2/src/person/Person.ts
@@ -0,0 +1,10 @@
+import * as U from '../utils/makeRandomNumber'
+import IPerson from "./IPerson";
+import {makeRandomNumber} from "../utils/makeRandomNumber";
+
+export default class Person implements IPerson {
+    constructor(public name: string, public age: number = makeRandomNumber()) {
+    }
+}
+
+export const makePerson = (name: string, age: number = U.makeRandomNumber()): IPerson => ({name, age});
