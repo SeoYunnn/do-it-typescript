d844e258733337a139e63d83d0d59fb2761ee111
seoyun-lee <20212025@o365.hanbat.ac.kr>
2023-09-08 00:47:54 +0900

    typescript ch03 : 객체와 타입

--- ch03/declaration.ts
+++ ch03/declaration.ts
@@ -2,3 +2,8 @@ let n: number = 1
 let b: boolean = true // or false
 let s: string = "hello"
 let o: object = {}
+
+n = 1
+b = false
+s = "hi"
+o = {name: "Jack", age: 32}
---
+++ ch03/template.ts
@@ -0,0 +1,3 @@
+let count = 10, message = "Count"
+let result = `${message} is ${count}`
+console.log(result)
